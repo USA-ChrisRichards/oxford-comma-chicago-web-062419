3f6550a6447a1e507a83a2b4eb4942673ad5ae70
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-18 01:48:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -10,7 +10,7 @@ def oxford_comma(array)
     str = arr.join(" ")
     str
   elsif array.size >= 3
-    last_element = "and #{array[-1]}"
+    array[-1] = "and #{array[-1]}"
     array.join(", ")
   end
 end
