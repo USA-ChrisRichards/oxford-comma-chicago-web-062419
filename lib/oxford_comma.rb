6dca134ff4a2a2c6c779d255b05953078962e8fa
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-18 01:50:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,14 +3,9 @@ def oxford_comma(array)
     array.pop
   elsif array.size == 2
     array.join(' and ')
-  elsif array.size == 3
-    str = array.join(", ")
-    arr = str.split(' ')
-    arr = arr.insert(2, "and")
-    str = arr.join(" ")
-    str
+  
   elsif array.size >= 3
-    array[-1] = "and #{array[-1]}"
-    array.join(", ")
+    array[-1] = "and #{array[-1]}" # reassign last element of array to "and dragon fruits"
+    array.join(", ") 
   end
 end
