727f9985e07134c3ffca12b8e84c410940063e74
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-18 01:18:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -15,7 +15,7 @@ def oxford_comma(array)
   x = array.size - 1
   arr = arr.insert(x, "and") 
   puts arr
-  str = arr.join(", ") 
+  str = arr.join(",") 
   puts str
   arr = str.split(',')
   print arr
