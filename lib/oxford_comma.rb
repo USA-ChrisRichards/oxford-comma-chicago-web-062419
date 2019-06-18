0386223c1458993d7152be9e981421d6f9b9ec3b
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-18 01:10:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.pop
+  elsif array.size == 2
+    array.join(' and ')
+  elsif array.size == 3
+    str = array.join(", ")
+    arr = str.split(' ')
+    arr = arr.insert(2, "and")
+    str = arr.join(" ")
+    str
+  elsif array.size > 3
+  str = array.join(", ")
+  arr = str.split(',')
+  x = array.size - 1
+  arr = arr.insert(x, "and")
+  str = arr.join(", ")
+  str
+  end
 end
